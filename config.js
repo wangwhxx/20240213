dea7068c46aa679aee189b6c1783b9f4d8583f9f
wangwhxx <156889687+wangwhxx@users.noreply.github.com>
2024-02-14 00:37:25 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'MRT in Singapore',
+            image: 'network_map_2022_November.png',
             description: 'The Mass Rapid Transit system, locally known by the initialism MRT, is a rapid transit system in Singapore.The network has since grown to span the length and breadth of the countrys main island in accordance with Singapores aim of developing a comprehensive rail network as the backbone of the countrys public transportation system,averaging a daily ridership of 3.4 million in 2019.',
             location: {
                 center: [103.845507,1.332462],
@@ -78,6 +79,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Getting Around by Public Transport',
+            image: 'the-new-trains-for-the.jpg',
             description: 'By Public Transport. MRT and bus systems have an extensive network of routes that will help you zip around the city. Most of our popular attractions are just a short walk away from an MRT station, making our trains a great way to get around.',
             location: {
                 center: [103.851401,1.290868],
@@ -143,6 +145,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Community Clubs',
+            image: 'katong-cc.jpg',
             description: 'The Singapore Community Club, also known as the Community Club (CC),serves as a common space for people of all races to come together, build friendships, and promote social bonding. CCs connect residents and provide various facilities, courses, and interest groups for community members.',
             location: {
                 center: [103.813678,1.335408],
@@ -207,6 +210,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Sports Facilities',
+            image: 'SingaporeSportsHub_Masthead.jpg',
             description: 'Sport Singapore believes that well-designed and efficiently run sports facilities will significantly open up opportunities for a more sporting, recreational and wellness lifestyle for all Singaporeans. ',
             location: {
                 center: [103.813678,1.335408],
